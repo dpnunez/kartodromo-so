118f7a954c1235d90d8c454c0237775a3bcd4acb
Daniel Núñez <daniel.portonunez@gmail..com>
2024-08-06 16:26:25 -0300

    feat: add base project

---
+++ src/kartodromo.c
@@ -0,0 +1,19 @@
+#include "kartodromo.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializa_kartodromo(Kartodromo *k) {
+    k->karts_disponiveis = NUM_KARTS;
+    k->capacetes_disponiveis = NUM_CAPACETES;
+    pthread_mutex_init(&k->mutex_karts, NULL);
+    pthread_mutex_init(&k->mutex_capacetes, NULL);
+    pthread_cond_init(&k->cond_karts, NULL);
+    pthread_cond_init(&k->cond_capacetes, NULL);
+}
+
+void destroi_kartodromo(Kartodromo *k) {
+    pthread_mutex_destroy(&k->mutex_karts);
+    pthread_mutex_destroy(&k->mutex_capacetes);
+    pthread_cond_destroy(&k->cond_karts);
+    pthread_cond_destroy(&k->cond_capacetes);
+}
---
+++ src/kartodromo.h
@@ -0,0 +1,26 @@
+#ifndef KARTODROMO_H
+#define KARTODROMO_H
+
+#include <pthread.h>
+
+#define NUM_KARTS 10
+#define NUM_CAPACETES 10
+
+typedef struct {
+    char nome[50];
+    int idade;
+} Piloto;
+
+typedef struct {
+    int karts_disponiveis;
+    int capacetes_disponiveis;
+    pthread_mutex_t mutex_karts;
+    pthread_mutex_t mutex_capacetes;
+    pthread_cond_t cond_karts;
+    pthread_cond_t cond_capacetes;
+} Kartodromo;
+
+void inicializa_kartodromo(Kartodromo *k);
+void destroi_kartodromo(Kartodromo *k);
+
+#endif // KARTODROMO_H
